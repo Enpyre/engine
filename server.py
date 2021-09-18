db085cc7af0ba351867f27b2488b719cf9680af7
Vitor Nere <vitor.nere@hotmail.com>
2021-09-18 11:29:06 -0300

    Adding cors

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 import os
 import src.engine
+import aiohttp_cors
 from aiohttp import web
 
 app = web.Application()
 
-app.router.add_static('/', './dist', show_index=True)
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+})
+
+cors.add(app.router.add_static('/', './dist', show_index=True))
 
 def main():
     os.system('python3 -m build')
