db40cfb8084f5770dbfdf560d5bdefb1bf52af06
Vitor Nere <vitor.nere@hotmail.com>
2021-10-02 14:57:57 -0300

    New sample

--- src/examples/sample_circle_by_keys.py
+++ src/examples/sample_circle_by_keys.py
@@ -1,17 +1,5 @@
 from src.engine import pyongine
 
-
-def update(time: float):
-    x = y = time%800
-    if not hasattr(pyongine, 'circle'):
-        pyongine.circle = pyongine.draw_circle(x, y, 100, '#ffffff')
-    else:
-        pyongine.circle.x = x
-        pyongine.circle.y = y
-
-pyongine.run(800, 800, '#000000', update)
-
-
 def update(time: float):
     SPEED = 3
     if not hasattr(pyongine, 'circle'):
