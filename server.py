0ed233129f3fc4e54216e99ed250d91f153f220d
Vitor Nere <vitor.nere@hotmail.com>
2022-06-15 23:24:26 -0300

    Not build if lint fail on server

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import os
+
 import aiohttp_cors
 from aiohttp import web
 
 app = web.Application()
 
-cors = aiohttp_cors.setup(app, defaults={
-    "*": aiohttp_cors.ResourceOptions(
+cors = aiohttp_cors.setup(
+    app,
+    defaults={
+        "*": aiohttp_cors.ResourceOptions(
             allow_credentials=True,
             expose_headers="*",
             allow_headers="*",
         )
-})
+    },
+)
 
-cors.add(app.router.add_static('/', './dist', show_index=True))
+cors.add(app.router.add_static("/", "./dist", show_index=True))
 
 
 def main():
-    os.system('poetry build')
-    web.run_app(app)
+    if os.system("flake8 && mypy src && poetry build") == 0:
+        web.run_app(app)
