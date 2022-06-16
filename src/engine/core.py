014144a37cbd178f81b9e05a30963bd9d10667c8
Vitor Nere <vitor.nere@hotmail.com>
2022-06-15 22:57:46 -0300

    Adding mypy

--- src/engine/core.py
+++ src/engine/core.py
@@ -3,7 +3,9 @@ from . import js_interface
 
 
 class Core:
-    __jsupdate = None
+    __jsupdate: Callable[['Core', float], None]
+    time: float
+    frame: int
 
     def key_pressed(self, key):
         return js_interface.keyPressed(key)
@@ -14,6 +16,6 @@ class Core:
             self.frame += 1
             self.__jsupdate(delta)
 
-    def get_update(self, jsupdate: Callable[[float], None]):
+    def get_update(self, jsupdate):
         self.__jsupdate = jsupdate
         return js_interface.create_proxy(self.__update)
